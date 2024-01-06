08d62e60f0e8e7152161213528d72c6d7155e2c6
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-01-07 01:33:54 +0300

    Code along completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,6 +9,22 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    program_list = []
+    
+    programs = json.loads(self.get_programs())
+    
+    for program in programs:
+      program_list.append(program['agency'])
+      
+    return program_list
+  
+programs = GetPrograms()
+programs_schools = programs.program_school()
 
-programs = GetPrograms().get_programs()
-print(programs)
+for school in set(programs_schools):
+    print(school)
+
+# programs = GetPrograms().get_programs()
+# print(programs)
